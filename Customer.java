2ff87c36a57c7e55c566a8e8ebdce8e4f68b7b24
Erik Thompson <erikthompson@Eriks-MacBook-Pro-2.local>
2025-09-22 16:59:35 -0700

    Initialized the Customer class, instantiating attributes, constructor, and elementary methods missing some logic.

---
+++ Customer.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class Customer{
+    private int customerId;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    // List of tickets a customer has
+    private List<Ticket> ticketList;
+    private float balance;
+
+    // constructor
+    public Customer{
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.ticketList = ticketList;
+        this.balance = 0;
+    }
+
+    // methods
+    // (unsure of args atp)
+    public void bookTicket(Flight flight, Seat seat, float price){
+        // is flight operable
+            // is seat available
+                ticketList.add(ticket)
+                balance += price;
+        // else ->
+        System.out.println("This seat or flight is not available")
+    }
+    public void cancelTicket(Ticket ticket){
+        // add logic
+    }
+
+    public void seeTickets(){
+        // add logic
+    }
+
+
+}
+
