fb5e1a6da0316495be35947c96841e9ad11483de
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-09-22 19:30:26 -0700

    Added attributes to abstract crew class, intialize constructor, getter setters  soon

--- Crew.java
+++ Crew.java
@@ -1,3 +1,10 @@
+import java.time.LocalDate;
+
 public abstract class Crew {
+    private final String employeeID;
+    private final String fullName;
+    private final LocalDate hiredOn;
+    private String baseAirport;
+
     
 }
