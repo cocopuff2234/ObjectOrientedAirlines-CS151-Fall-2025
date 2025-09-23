df4b0faf9bd7ffaaeede1baabde2c34e23d91667
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-09-22 19:42:42 -0700

    Added getters and constructor for flight class

--- Flight.java
+++ Flight.java
@@ -6,6 +6,31 @@ public class Flight {
     private final String destination;
     private final LocalDateTime departureUTC;
 
+    public Flight(String flightNumber, String origin, String destination, LocalDateTime departureUTC) {
+        this.flightNumber = flightNumber;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureUTC = departureUTC;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public LocalDateTime getDepartureUTC() {
+        return departureUTC;
+    }
+
+    
+
     
 
 
