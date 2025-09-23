25d122d73650936137242c49ee8bc58caff9220d
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-09-22 19:41:14 -0700

    Added preliminary attributes for flight class

--- Flight.java
+++ Flight.java
@@ -1,3 +1,12 @@
+import java.time.LocalDateTime;
+
 public class Flight {
+    private final String flightNumber;
+    private final String origin;
+    private final String destination;
+    private final LocalDateTime departureUTC;
+
+    
+
 
 }
