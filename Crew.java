4bd8e067880939c601e6284adf0a11ab094702cc
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-09-22 19:34:50 -0700

    Added getters and setters along with constructor for crew

--- Crew.java
+++ Crew.java
@@ -6,5 +6,33 @@ public abstract class Crew {
     private final LocalDate hiredOn;
     private String baseAirport;
 
+    protected Crew(String employeeID, String fullName, LocalDate hiredOn, String baseAirport) {
+        this.employeeID = employeeID;
+        this.fullName = fullName;
+        this.hiredOn = hiredOn;
+        this.baseAirport = baseAirport;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public LocalDate getHiredOn() {
+        return hiredOn;
+    }
+
+    public String getBaseAirport() {
+        return baseAirport;
+    }
+
+    public void setBaseAirport(String baseAirport) {
+        this.baseAirport = baseAirport;
+    }
+
+    
     
 }
