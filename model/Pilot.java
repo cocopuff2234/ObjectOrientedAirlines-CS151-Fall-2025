3b7436fe830b18454ad215c0b0d290c52b4ba7ec
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-10-05 14:19:32 -0700

    Added some planetypes, created Pilot.java

---
+++ model/Pilot.java
@@ -0,0 +1,11 @@
+import model.Crew;
+import model.enums.PlaneType;
+import model.enums.PilotRank;
+import model.enums.CrewStatus;
+
+import java.util.LocalDate;
+import java.util.*;
+public class Pilot extends Crew {
+
+    
+}
