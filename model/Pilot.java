8e3991d276ef8a4ec4d3ce283eab9308ec154b0a
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-10-05 14:34:32 -0700

    Added override moethods from Crew on Pilot class

--- model/Pilot.java
+++ model/Pilot.java
@@ -1,5 +1,6 @@
 import model.Crew;
 import model.enums.PlaneType;
+import model.enums.Role;
 import model.enums.PilotRank;
 import model.enums.CrewStatus;
 
@@ -7,7 +8,7 @@ import java.time.LocalDate;
 import java.util.*;
 public class Pilot extends Crew {
     private PilotRank rank;
-    private final Set<PlaneType> typeRatings = new HashSet<>();
+    private final Set<PlaneType> typeRatings = new HashSet<>(); 
     private int totalFlightHours;
 
 
@@ -20,6 +21,47 @@ public class Pilot extends Crew {
         }
         this.totalFlightHours = Math.max(0, totalFlightHours);
     }
+
+
+    public PilotRank getRank() {
+        return rank;
+    }
+
+
+    public void setRank(PilotRank rank) {
+        this.rank = rank;
+    }
+
+
+    public Set<PlaneType> getTypeRatings() {
+        return Collections.unmodifiableSet(PlaneType);
+    }
+
+
+    public int getTotalFlightHours() {
+        return totalFlightHours;
+    }
+
+
+    public void setTotalFlightHours(int totalFlightHours) {
+        this.totalFlightHours = totalFlightHours;
+    }
+
+
+    @Override
+    public boolean canOperate(PlaneType type) {
+        // From Crew, check if pilot is available and certified for flight
+        return getStatus() == CrewStatus.AVAILABLE && typeRatings.contains(type);
+    }
+
+
+    @Override
+    public Role getRole() {
+        // Implements abstract from crew, either CAPTAIN or FIRST OFFICER
+        return Role.PILOT;
+    }
+
+    
 
     
     
