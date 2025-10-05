6eeab31e483bfe5ee45c52cbcea4e3c480fe34ef
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-10-05 16:07:43 -0700

    Added comments to Pilot for readability

---
+++ model/FlightAttendant.java
@@ -0,0 +1,18 @@
+package model;
+
+import model.enums.PlaneType;
+import model.enums.Role;
+import model.enums.FAPosition;
+import model.enums.CrewStatus;
+
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ * Represents a flight attendant who is a type of crew member.
+ * Each attendant has a position (Lead or Junior) and certifications
+ * for which aircraft types they can serve on.
+ */
+public class FlightAttendant extends Crew {
+
+}
