724bcbdeabc83d909564f243c89d3991d08ebcbe
Hazelette <62589987+Hazelette@users.noreply.github.com>
2025-10-07 08:02:52 -0700

    Added new attributes to flight: planeType to minAttendants

--- model/Flight.java
+++ model/Flight.java
@@ -1,5 +1,10 @@
 package model;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import model.enums.PlaneType;
 
 public class Flight {
     private final String flightNumber;
@@ -9,7 +14,15 @@ public class Flight {
     private final LocalDateTime departureUTC;
     private final LocalDateTime arrivalUTC;
     private String gate;
+    private final PlaneType planeType;
+    private Pilot captain;
+    private Pilot firstOfficer;
+    private final List<FlightAttendant> attendants = new ArrayList<>();
+    private final int minAttendants;
 
+    
+
+    /* 
     public Flight(String flightNumber, String airline, String origin, String destination, LocalDateTime departureUTC, LocalDateTime arrivalUTC) {
         this.flightNumber = flightNumber;
         this.airline = airline;
@@ -17,8 +30,21 @@ public class Flight {
         this.destination = destination;
         this.departureUTC = departureUTC;
         this.arrivalUTC = arrivalUTC;
+        this.planeType = Objects.requireNonNull(planeType);
     }
+    */
 
+    public Flight(String flightNumber, String airline, String origin, String destination, LocalDateTime departureUTC,
+            LocalDateTime arrivalUTC, PlaneType planeType, int minAttendants) {
+        this.flightNumber = flightNumber;
+        this.airline = airline;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureUTC = departureUTC;
+        this.arrivalUTC = arrivalUTC;
+        this.planeType = planeType;
+        this.minAttendants = minAttendants;
+    }
     public String getFlightNumber() { return flightNumber; }
     public String getAirline() { return airline; }
     public String getOrigin() { return origin; }
@@ -27,5 +53,6 @@ public class Flight {
     public LocalDateTime getArrivalUTC() { return arrivalUTC; }
     public String getGate() { return gate; }
     public void setGate(String gate) { this.gate = gate; }
+
 
 }
